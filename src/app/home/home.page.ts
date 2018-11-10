cf1a469350ac328f528eaec9e832923bc2922a44
Evan <devillordking@gmail.com>
2018-11-11 10:10:44 +1100

    add cordova

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {}
+
+export class HomePage {
+  constructor(private camera: Camera) { }
+  
+  capture() {
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      console.log(base64Image)
+     }, (err) => {
+      // Handle error
+     });
+  }
+}
+
+const options: CameraOptions = {
+  quality: 100,
+  destinationType: this.camera.DestinationType.FILE_URI,
+  encodingType: this.camera.EncodingType.JPEG,
+  mediaType: this.camera.MediaType.PICTURE
+}
